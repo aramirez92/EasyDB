b974c57fa48fb6da2d08adbd5183fc835c0bde7f
Alfredo Ramirez <alfredo@ciber-adictos.com>
2014-07-09 10:27:32 -0600

    Update EasyDB.php

--- EasyDB.php
+++ EasyDB.php
@@ -40,8 +40,7 @@ Class EasyDB {
 		$options = array(
 		   PDO::ATTR_PERSISTENT => true, 
 		   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-		   PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
-		   PDO::ATTR_EMULATE_PREPARES => false
+		   PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
 		);
 
 		/* Create a new PDO instanace */
